68682b1da0f3f8aab52b40f9da0ad1021403c241
vasi <vasi.che@gmail.com>
2026-02-10 06:45:26 +0500

    "public" for VacancyServiceTest.java

--- src/test/java/ru/perm/v/dagger_rest/services/VacancyServiceTest.java
+++ src/test/java/ru/perm/v/dagger_rest/services/VacancyServiceTest.java
@@ -8,7 +8,7 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-class VacancyServiceTest {
+public class VacancyServiceTest {
 
     @Test
     void getVacancies() {
