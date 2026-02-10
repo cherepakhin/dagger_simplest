fd557c9cc9ab5b763ffe9088686cbda1230fe228
vasi <vasi.che@gmail.com>
2026-02-10 06:43:24 +0500

    add check length message for EchoResource.java

--- src/main/java/ru/perm/v/dagger_rest/resources/EchoResource.java
+++ src/main/java/ru/perm/v/dagger_rest/resources/EchoResource.java
@@ -12,8 +12,15 @@ public class EchoResource {
     @GET
     @Path("/{message}")
     public Response getMessage(@PathParam("message") String message) {
-        if (message == null || message.isEmpty()) {
-            return Response.status(Response.Status.BAD_REQUEST).build();
+        if (message == null) {
+            return Response.status(Response.Status.BAD_REQUEST)
+                    .entity("Message must be not null")
+                    .build();
+        }
+        if (message.length() < 4) {
+            return Response.status(Response.Status.BAD_REQUEST)
+                    .entity("Length of the Message must be greater than 4")
+                    .build();
         }
         return Response.ok(message).build();
     }
--- src/test/java/ru/perm/v/dagger_rest/echo/EchoResourceTest.java
+++ src/test/java/ru/perm/v/dagger_rest/echo/EchoResourceTest.java
@@ -11,14 +11,38 @@ public class EchoResourceTest {
     @Test
     public void testEchoMessage() {
         EchoResource echoResource = new EchoResource();
+
         Response response = echoResource.getMessage("Hello World");
+
         assertEquals("Hello World", response.getEntity());
     }
 
     @Test
     public void testEchoStatus() {
         EchoResource echoResource = new EchoResource();
+
         Response response = echoResource.getMessage("Hello World");
+
         assertEquals(200, response.getStatus());
+    }
+
+    @Test
+    public void testNullMessage() {
+        EchoResource echoResource = new EchoResource();
+
+        Response response = echoResource.getMessage(null);
+
+        assertEquals(400, response.getStatus());
+        assertEquals("Message must be not null", response.getEntity());
+    }
+
+    @Test
+    public void testForShortMessage() {
+        EchoResource echoResource = new EchoResource();
+
+        Response response = echoResource.getMessage("aaa");
+
+        assertEquals(400, response.getStatus());
+        assertEquals("Length of the Message must be greater than 4", response.getEntity());
     }
 }
